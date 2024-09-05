23c5bf67966a6ca55f2ec3f51ae2c3242c4d889b
n44vz <nvz.classes@gmail.com>
2024-09-05 13:44:31 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,28 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+# Diccionario de mapeo de nombres de estados a códigos
+state_codes = {
+    'Alabama': 'AL', 'Alaska': 'AK', 'Arizona': 'AZ', 'Arkansas': 'AR', 'California': 'CA',
+    'Colorado': 'CO', 'Connecticut': 'CT', 'Delaware': 'DE', 'Florida': 'FL', 'Georgia': 'GA',
+    'Hawaii': 'HI', 'Idaho': 'ID', 'Illinois': 'IL', 'Indiana': 'IN', 'Iowa': 'IA',
+    'Kansas': 'KS', 'Kentucky': 'KY', 'Louisiana': 'LA', 'Maine': 'ME', 'Maryland': 'MD',
+    'Massachusetts': 'MA', 'Michigan': 'MI', 'Minnesota': 'MN', 'Mississippi': 'MS', 'Missouri': 'MO',
+    'Montana': 'MT', 'Nebraska': 'NE', 'Nevada': 'NV', 'New Hampshire': 'NH', 'New Jersey': 'NJ',
+    'New Mexico': 'NM', 'New York': 'NY', 'North Carolina': 'NC', 'North Dakota': 'ND', 'Ohio': 'OH',
+    'Oklahoma': 'OK', 'Oregon': 'OR', 'Pennsylvania': 'PA', 'Rhode Island': 'RI', 'South Carolina': 'SC',
+    'South Dakota': 'SD', 'Tennessee': 'TN', 'Texas': 'TX', 'Utah': 'UT', 'Vermont': 'VT',
+    'Virginia': 'VA', 'Washington': 'WA', 'West Virginia': 'WV', 'Wisconsin': 'WI', 'Wyoming': 'WY',
+    'District of Columbia': 'DC'
+}
+
 # Cargar los datos
 @st.cache_data
 def load_data():
-    df = pd.read_csv('.data/ventas.csv')
-    # Asegurarse de que 'State' esté capitalizado correctamente
+    df = pd.read_csv('data/ventas.csv')
     df['State'] = df['State'].str.title()
+    # Convertir nombres de estados a códigos
+    df['State_Code'] = df['State'].map(state_codes)
     return df
 
 # Cargar los datos
@@ -30,22 +46,23 @@ st.plotly_chart(fig_scatter)
 
 # Mapa de ventas por estado
 st.subheader('Mapa de Ventas por Estado')
-sales_by_state = df.groupby('State')['Sales'].sum().reset_index()
+sales_by_state = df.groupby(['State', 'State_Code'])['Sales'].sum().reset_index()
 
 # Verificar los estados únicos
 unique_states = sales_by_state['State'].unique()
 st.write(f"Estados únicos en los datos: {', '.join(unique_states)}")
 
 fig_map = px.choropleth(sales_by_state, 
-                        locations='State', 
+                        locations='State_Code', 
                         locationmode="USA-states", 
                         color='Sales', 
                         scope="usa", 
-                        color_continuous_scale="Viridis")
+                        color_continuous_scale="Viridis",
+                        hover_name='State')
 
 # Ajustar la configuración del mapa
 fig_map.update_layout(
-    geo_scope='usa',  # Enfocar en USA
+    geo_scope='usa',
     geo=dict(
         showcoastlines=True,
         coastlinecolor="Black",
